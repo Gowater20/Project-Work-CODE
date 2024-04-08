c0c151e7a7996d116a56a3aac67856a273b009c8
Fabio Vallacqua <157326419+Gowater20@users.noreply.github.com>
2024-04-08 11:41:25 +0200

    Add files via upload

---
+++ src/dbconnection.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import {app} from './index';
+
+
+const CONNECTION_URL: string = 'mongodb://localhost:27017/ProjectWorkCode';
+const PORT = 3000;
+
+const DB = async () => {
+    try {
+        await mongoose.connect(CONNECTION_URL);
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`server is online at http://localhost:${PORT}`);
+    });
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+
+export default DB();
