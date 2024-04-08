7c39d4230766da331445c38d9cb78971a007a57c
IPVale <valeria.imbrogiop@gmail.com>
2024-04-08 12:40:24 +0200

    product models/type

---
+++ src/models/product.models.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+import { IProduct } from "../types/product.type";
+
+const productSchema = new mongoose.Schema<IProduct>(
+	{
+		id: {
+			type: String,
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		brand: {
+			type: String,
+			required: true,
+		},
+		quantity: {
+			type: Number,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		sale: {
+			type: Number,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
